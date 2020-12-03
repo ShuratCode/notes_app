874d74cb15e9c7dc3f5ae406745e63dd6c0c7065
Shaked Eyal <shaked.eyal@protonmail.com>
2020-12-03 22:21:06 +0200

    Start parsing arguments with yargs

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+// Customise yargs version
+yargs.version('1.1.0')
 
-if (command === 'add')
-{
-    console.log('Adding node')
-}
-else if (command === 'remove')
-{
-    console.log('Remove note')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'The note content',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Removing note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function () {
+        console.log('Listing all notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading the note')
+    }
+})
+
+yargs.parse()
